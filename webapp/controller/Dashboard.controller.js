5dd12f2ea2da292e0f55405d967ae63020fc1389
Thrisha Krishna <thrisha.krishna@accenture.com>
2024-09-13 11:42:14 +0000

    Day2-2nd commit

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,12 +1,34 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller,JSONModel) {
    "use strict";
 
    return Controller.extend("ui5.mock.controller.Dashboard", {
        onInit: function () {
-           // Initialization code for the dashboard
-           console.log("Dashboard view initialized");
-       }
+        var oModel = new JSONModel("MockData.json");
+        this.getView().setModel(oModel);
+       },
+       onFilter: function () {
+        var name = this.getView().byId("nameInput").getValue().toLowerCase();
+        var eid = this.getView().byId("eidInput").getValue().toLowerCase();
+        var status = this.getView().byId("statusInput").getSelectedKey();
+
+        var aFilters = [];
+        if (name) {
+            aFilters.push(new sap.ui.model.Filter("FirstName", sap.ui.model.FilterOperator.Contains, name));
+        }
+        if (eid) {
+            aFilters.push(new sap.ui.model.Filter("EmployeeID", sap.ui.model.FilterOperator.Contains, eid));
+        }
+        if (status) {
+            aFilters.push(new sap.ui.model.Filter("Status", sap.ui.model.FilterOperator.EQ, status));
+        }
+
+        var oTable = this.getView().byId("employeeTable");
+        var oBinding = oTable.getBinding("items");
+        oBinding.filter(aFilters);
+    },
    });
 });
